cff1359bf27c54fd2a798b03cc76f43df7314264
rirefat <rirefat.official@gmail.com>
2024-05-24 20:27:25 +0600

    done project basic setup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 const app: Application = express();
 
+// parsers
+app.use(express.json());
+app.use(cors())
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+    res.send('Welcome to CampusConnect')
 })
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
-import config from "./app/config"
-import app from "./app"
+import mongoose from 'mongoose';
+import config from "./app/config";
+import app from "./app";
 
-app.listen(config.port, () => {
-    console.log(`Example app listening on port ${config.port}`)
-})
+async function main() {
+    try {
+        await mongoose.connect(config.db_url as string);
+        app.listen(config.port, () => {
+            console.log(`Example app listening on port ${config.port}`)
+        })
+    } catch (err) {
+        console.log(err);
+    }
+}
+main();
