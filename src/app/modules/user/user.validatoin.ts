9b91b934b4e0210fe5380123a04ac4860ff94c15
rirefat <rirefat.official@gmail.com>
2024-05-30 03:13:12 +0600

    refactoring the user validation

--- src/app/modules/user/user.validatoin.ts
+++ src/app/modules/user/user.validatoin.ts
@@ -1,10 +1,25 @@
 import { z } from "zod";
 
 export const userValidationSchema = z.object({
-    id: z.string({ required_error: "User id is required" }),
-    password: z.string().min(8, { message: "Password should be more than 8 characters" }),
-    needPasswordChange: z.boolean().optional().default(true),
-    role: z.enum(['student', 'faculty', 'admin']),
-    status: z.enum(['in-progress', 'blocked']).optional().default("in-progress"),
-    isDeleted: z.boolean().optional().default(false)
+    // id will be generated automatically in server
+    // id: z
+    //     .string({ required_error: "User id is required" }),
+    password: z
+        .string({invalid_type_error: 'Password must be string'})
+        .min(8, { message: "Password should be more than 8 characters" })
+        .optional(),
+    // needPasswordChange: z
+    //     .boolean()
+    //     .optional()
+    //     .default(true),
+    // role: z
+    //     .enum(['student', 'faculty', 'admin']),
+    // status: z
+    //     .enum(['in-progress', 'blocked'])
+    //     .optional()
+    //     .default("in-progress"),
+    // isDeleted: z
+    //     .boolean()
+    //     .optional()
+    //     .default(false)
 });
