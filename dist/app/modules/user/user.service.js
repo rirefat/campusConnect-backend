b6abc2df664d18cdd1ff5007b430f46370fda95b
rirefat <rirefat.official@gmail.com>
2024-06-10 16:15:32 +0600

    implement transaction and rollback for creating new student

--- dist/app/modules/user/user.service.js
+++ dist/app/modules/user/user.service.js
@@ -13,34 +13,51 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.userServices = void 0;
+const mongoose_1 = __importDefault(require("mongoose"));
 const config_1 = __importDefault(require("../../config"));
 const academicSemester_model_1 = require("../academicSemester/academicSemester.model");
 const student_model_1 = require("../student/student.model");
 const user_model_1 = require("./user.model");
 const user_utils_1 = require("./user.utils");
-const createStudentIntoDB = (password, studentData) => __awaiter(void 0, void 0, void 0, function* () {
+const createStudentIntoDB = (password, payload) => __awaiter(void 0, void 0, void 0, function* () {
     // create user object
     // const user = {} as NewUser;
     const userData = {};
     // find academic semester info
-    const admissionSemester = yield academicSemester_model_1.AcademicSemesterModel.findById(studentData.admissionSemester);
+    const admissionSemester = yield academicSemester_model_1.AcademicSemesterModel.findById(payload.admissionSemester);
     if (!admissionSemester) {
         throw new Error('Admission semester not found');
     }
-    // set user id manually
-    userData.id = yield (0, user_utils_1.generateStudentId)(admissionSemester);
     // if password is not given, use default password
     userData.password = password || config_1.default.user_pass;
     // set student role
     userData.role = 'student';
-    // create user
-    const newUser = yield user_model_1.UserModel.create(userData);
-    // create a student
-    if (Object.keys(newUser).length) {
-        studentData.id = newUser.id;
-        studentData.user = newUser._id;
-        const newStudent = yield student_model_1.StudentModel.create(studentData);
+    const session = yield mongoose_1.default.startSession();
+    try {
+        session.startTransaction();
+        // set user id manually
+        userData.id = yield (0, user_utils_1.generateStudentId)(admissionSemester);
+        // create user: (transaction-1)
+        const newUser = yield user_model_1.UserModel.create([userData], { session });
+        if (!newUser.length) {
+            throw new Error("Failed to create new user!!");
+        }
+        payload.id = newUser[0].id;
+        payload.user = newUser[0]._id;
+        // create user: (transaction-2)
+        const newStudent = yield student_model_1.StudentModel.create([payload], { session });
+        if (!newStudent.length) {
+            throw new Error("Failed to create new student!!");
+        }
+        yield session.commitTransaction();
+        yield session.endSession();
         return newStudent;
+    }
+    catch (err) {
+        yield session.abortTransaction();
+        yield session.endSession();
+        throw new Error("Failed to create new student!!");
+        // console.log(err);
     }
 });
 exports.userServices = {
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import config from "../../config";
 import { AcademicSemesterModel } from "../academicSemester/academicSemester.model";
 import { TStudent } from "../student/student.interface";
@@ -6,37 +7,57 @@ import { TUser } from "./user.interface";
 import { UserModel } from "./user.model";
 import { generateStudentId } from "./user.utils";
 
-const createStudentIntoDB = async (password: string, studentData: TStudent) => {
+const createStudentIntoDB = async (password: string, payload: TStudent) => {
     // create user object
     // const user = {} as NewUser;
     const userData: Partial<TUser> = {};
 
     // find academic semester info
-    const admissionSemester = await AcademicSemesterModel.findById(studentData.admissionSemester);
+    const admissionSemester = await AcademicSemesterModel.findById(payload.admissionSemester);
     if (!admissionSemester) {
         throw new Error('Admission semester not found');
     }
 
-    // set user id manually
-    userData.id = await generateStudentId(admissionSemester);
-
     // if password is not given, use default password
     userData.password = password || (config.user_pass as string);
-
     // set student role
     userData.role = 'student'
 
-    // create user
-    const newUser = await UserModel.create(userData);
+    const session = await mongoose.startSession();
 
-    // create a student
-    if (Object.keys(newUser).length) {
-        studentData.id = newUser.id;
-        studentData.user = newUser._id;
+    try {
+        session.startTransaction();
+        // set user id manually
+        userData.id = await generateStudentId(admissionSemester);
 
-        const newStudent = await StudentModel.create(studentData);
+        // create user: (transaction-1)
+        const newUser = await UserModel.create([userData], { session });
+
+        if (!newUser.length) {
+            throw new Error("Failed to create new user!!")
+        }
+
+        payload.id = newUser[0].id;
+        payload.user = newUser[0]._id;
+
+        // create user: (transaction-2)
+        const newStudent = await StudentModel.create([payload], { session });
+
+        if (!newStudent.length) {
+            throw new Error("Failed to create new student!!")
+        }
+
+        await session.commitTransaction();
+        await session.endSession();
+
         return newStudent;
+    } catch (err) {
+        await session.abortTransaction();
+        await session.endSession();
+        throw new Error("Failed to create new student!!")
+        // console.log(err);
     }
+
 }
 
 export const userServices = {
